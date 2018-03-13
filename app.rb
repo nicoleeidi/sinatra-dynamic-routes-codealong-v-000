b95ee9fd02f5f4b6c2a2b98fe119893e690e303b
Nicole Eidi <nicoleeidi1@gmail.com>
2018-03-13 20:11:20 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -17,8 +17,8 @@ get '/goodbye/:name' do
   "Goodbye, #{@name}."
   end
 get '/multiply/:num1/:num2' do
-  @num1= params[:num1]
-  @num2= params[:num2]
+  @num1= params[:num1].to_i
+  @num2= params[:num2].to_i
   binding.pry
   "#{num1 * num2}"
 
